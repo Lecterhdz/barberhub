08c67c15f0d3e7d87980119a1dd48a7387c3a415
Lecterhdz <dialectycam@gmail.com>
2026-04-07 14:11:14 -0600

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -8,7 +8,7 @@ import { app } from './src/core/app.js';
 
 console.log('🧭 Router cargado');
 
-export const router = {
+export const routes = {
     '/auth': { feature: 'auth', titulo: 'Iniciar Sesión', requiereAuth: false, permisos: ['public'] },
     '/dashboard': { feature: 'dashboard', titulo: 'Dashboard', requiereAuth: true, permisos: ['todos'] },
     '/404': { feature: 'auth', titulo: 'Error', requiereAuth: false, permisos: ['public'] }
